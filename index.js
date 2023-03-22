d11374200e9df4bd343002403384ce2375d49d2f
thindHarminder <121890692+thindHarminder@users.noreply.github.com>
2023-03-22 11:02:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,24 @@ import $ from 'https://cdn.skypack.dev/pin/blingblingjs@v2.3.0-e2PTciHn3Q19fSwvw
 import Toast from 'https://cdn.jsdelivr.net/gh/thindHarminder/toast@main/toast.js'
 
 
+
+$('#actions').on('click', () => {
+  Toast(randomAction())
+})
+
+$('#spells').on('click', async () => {
+  const toastText = document.getElementById('textToast').value.trim();
+  if (toastText) {
+    await Toast(toastText);
+  } else {
+    await Toast(randomSpell());
+  }
+  console.log('poof');
+});
+
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
