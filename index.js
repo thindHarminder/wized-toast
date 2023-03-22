dbc32ded407626901f2fa3104c7532f854689134
thindHarminder <121890692+thindHarminder@users.noreply.github.com>
2023-03-22 15:23:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,6 @@ import $ from 'https://cdn.skypack.dev/pin/blingblingjs@v2.3.0-e2PTciHn3Q19fSwvw
 import Toast from 'https://cdn.jsdelivr.net/gh/thindHarminder/toast@main/toast.js'
 
 
-let prevToastState = false;
-
-function onToastStateChange() {
-  if (toaststate && !prevToastState) {
-    const toastText = toastmessage.value.trim();
-    await Toast(toastText);
-  }
-  prevToastState = toaststate;
-}
-
-// Call the onToastStateChange function whenever toaststate is updated
-toaststate = true;
-onToastStateChange();
-
 
 
 
