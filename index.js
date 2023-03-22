f1539d679ee91df67fb38761c921472c2949eb41
thindHarminder <121890692+thindHarminder@users.noreply.github.com>
2023-03-22 15:09:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,33 +2,10 @@ import $ from 'https://cdn.skypack.dev/pin/blingblingjs@v2.3.0-e2PTciHn3Q19fSwvw
 import Toast from 'https://cdn.jsdelivr.net/gh/thindHarminder/toast@main/toast.js'
 
 
-
-
-
-let toaststate = false; // assume initial state is false
-
-function showToast() {
-  if (toaststate) {
-    const toastText = toastmessage.trim();
-    if (toastText) {
-      Toast(toastText).then(() => {
-        toaststate = false; // reset toaststate to false after the Toast is done
-      });
-    }
-  }
-}
-
-// whenever toaststate changes, call showToast
-Object.defineProperty(window, 'toaststate', {
-  set(value) {
-    toaststate = value;
-    showToast();
-  },
-  get() {
-    return toaststate;
-  }
+$('#spells').on('click', async () => {
+  const toastText = document.getElementById('textToast').value.trim();
+  await Toast(toastText);
 });
-
 
 
 
