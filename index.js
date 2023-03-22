9e5ed367efb1af36d9fb0cb91fa5df38898f4b61
thindHarminder <121890692+thindHarminder@users.noreply.github.com>
2023-03-22 15:35:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-window.onload = async () => {  
+
     import $ from 'https://cdn.skypack.dev/pin/blingblingjs@v2.3.0-e2PTciHn3Q19fSwvwGKy/mode=imports,min/optimized/blingblingjs.js'
 import Toast from 'https://cdn.jsdelivr.net/gh/thindHarminder/toast@main/toast.js'
 
 
-
+window.onload = async () => {  
 $('#actions').on('click', () => {
   Toast(randomAction())
 })
