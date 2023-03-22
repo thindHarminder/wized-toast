fa3b2562be0e4c36771143484dd6222249866d48
thindHarminder <121890692+thindHarminder@users.noreply.github.com>
2023-03-22 15:31:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,23 @@ import Toast from 'https://cdn.jsdelivr.net/gh/thindHarminder/toast@main/toast.j
 
 
 
+$('#actions').on('click', () => {
+  Toast(randomAction())
+})
+
+$('#spells').on('click', async () => {
+  const toastText = document.getElementById('textToast').value.trim();
+  if (toastText) {
+    await Toast(toastText);
+  } else {
+    await Toast(randomSpell());
+  }
+  console.log('poof');
+});
 
 
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
