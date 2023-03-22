05c2cc3b0cbcc8dbc3116622971ca0146f17cd27
thindHarminder <121890692+thindHarminder@users.noreply.github.com>
2023-03-22 15:34:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.onload = async () => {  
-    mport $ from 'https://cdn.skypack.dev/pin/blingblingjs@v2.3.0-e2PTciHn3Q19fSwvwGKy/mode=imports,min/optimized/blingblingjs.js'
+    import $ from 'https://cdn.skypack.dev/pin/blingblingjs@v2.3.0-e2PTciHn3Q19fSwvwGKy/mode=imports,min/optimized/blingblingjs.js'
 import Toast from 'https://cdn.jsdelivr.net/gh/thindHarminder/toast@main/toast.js'
 
 
