f22c7b02f5ab8d9224dd1bb6ff8c0b25b36eba24
thindHarminder <121890692+thindHarminder@users.noreply.github.com>
2023-03-22 10:56:11 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+import $ from 'https://cdn.skypack.dev/pin/blingblingjs@v2.3.0-e2PTciHn3Q19fSwvwGKy/mode=imports,min/optimized/blingblingjs.js'
+import Toast from 'https://cdn.jsdelivr.net/gh/thindHarminder/toast@main/toast.js'
+
+
