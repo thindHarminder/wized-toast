7c9d5e9af6caf342553b2f6152aa979ac7d61b8b
thindHarminder <121890692+thindHarminder@users.noreply.github.com>
2023-03-23 08:23:49 -0700

    Updates formula

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 
-    import $ from 'https://cdn.skypack.dev/pin/blingblingjs@v2.3.0-e2PTciHn3Q19fSwvwGKy/mode=imports,min/optimized/blingblingjs.js'
+import $ from 'https://cdn.skypack.dev/pin/blingblingjs@v2.3.0-e2PTciHn3Q19fSwvwGKy/mode=imports,min/optimized/blingblingjs.js'
 import Toast from 'https://cdn.jsdelivr.net/gh/thindHarminder/toast@main/toast.js'
 
 
 
-
+const newValue = "Empty";
 window.onload = async () => {  
   Wized.data.listen("v.toastmessage", async () => {    
-      const newValue = await Wized.data.get("v.toastmessage"); // Get new value    
+      newValue = await Wized.data.get("v.toastmessage"); // Get new value    
       console.log("Value of v.toastmessage changed to: ", newValue); // Console log new value  
-      $('#spells').on('click', async () => {
-    
-        const toastText = newValue
-        if (toastText) {
-          await Toast(toastText);
-        } else {
-          console.log('toasttext not defined');
-        }
-        console.log('poof');
-      });
+      
   });
 }
+$('#spells').on('click', async () => {
+    
+  const toastText = newValue
+  if (toastText) {
+    await Toast(toastText);
+  } else {
+    console.log('toasttext not defined');
+  }
+  console.log('poof');
+});
 
 
 
